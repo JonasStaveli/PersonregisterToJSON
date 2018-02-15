437244756446bcc3af7888f07be7801fbe76f950
Jonas Staveli <staveli.jonas@gmail.com>
2018-02-15 20:07:54 +0100

    Added some comments.

--- PersonregisterToJSON/Controllers/PersonregisterController.cs
+++ PersonregisterToJSON/Controllers/PersonregisterController.cs
@@ -18,24 +18,30 @@ namespace PersonregisterToJSON.Controllers
         [HttpGet]
         public HttpResponseMessage Get(String id)
         {
+            //Connect to person service
             PersonServiceClient client = new PersonServiceClient();
             client.ClientCredentials.UserName.UserName = "test";
             client.ClientCredentials.UserName.Password = "BF32511";
-            LookupParameters par = new LookupParameters();
-            par.NIN = id;
-            Person xml = client.GetPerson(par);
+
+            //Add id to lookupparameters and fetch data
+            LookupParameters lookupParameters = new LookupParameters();
+            lookupParameters.NIN = id;
+            Person xml = client.GetPerson(lookupParameters);
 
             client.Close();
 
+            //serialize Person data to string
             var stringwriter = new System.IO.StringWriter();
             var serializer = new XmlSerializer(xml.GetType());
             serializer.Serialize(stringwriter, xml);
 
+            //convert from xml to json
             XmlDocument doc = new XmlDocument();
             doc.LoadXml(stringwriter.ToString());
 
             var json = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented, false);
 
+            //return json
             var res = Request.CreateResponse(HttpStatusCode.OK);
             res.Content = new StringContent(json, Encoding.UTF8, "application/json");
 
